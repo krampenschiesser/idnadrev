e3d76d9ccca953e310c64975840874c715308c1b
scar <krampenschiesser@freenet.de>
2013-10-29 13:31:12 +0100

    first shot of application's launcher

---
+++ libs/application/src/main/java/de/ks/application/Launcher.java
@@ -0,0 +1,12 @@
+package de.ks.application;
+/*
+ * Created by Christian Loehnert
+ * Krampenschiesser@freenet.de
+ * All rights reserved by now, license may come later.
+ */
+
+/**
+ *
+ */
+public class Launcher {
+}
