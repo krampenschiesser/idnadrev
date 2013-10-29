2b36f7bcd3b3d465cc72bbf7e7ced27be94a8e32
scar <krampenschiesser@freenet.de>
2013-10-29 18:57:51 +0100

    added deadevent handler to log missed events

--- pc/libs/eventsystem/src/main/java/de/ks/eventsystem/EventSystem.java
+++ pc/libs/eventsystem/src/main/java/de/ks/eventsystem/EventSystem.java
@@ -1,21 +1,28 @@
 package de.ks.eventsystem;
-
-import com.google.common.eventbus.EventBus;
-
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-
-/**
+/*
  * Created by Christian Loehnert
  * Krampenschiesser@freenet.de
  * All rights reserved by now, license may come later.
  */
+
+import com.google.common.eventbus.DeadEvent;
+import com.google.common.eventbus.EventBus;
+import com.google.common.eventbus.Subscribe;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 public class EventSystem {
+  private static final Logger log = LogManager.getLogger(EventSystem.class);
+
   public static final EventBus bus;
 
   static {
-    ExecutorService executor = Executors.newWorkStealingPool();
-//    bus = new AsyncEventBus(executor);
     bus = new EventBus();
+    bus.register(new EventSystem());
+  }
+
+  @Subscribe
+  public void onDeadEvent(DeadEvent dead) {
+    log.warn("No handler for event \"{}\" found. Contents: {}", dead.getEvent().getClass().getSimpleName(), dead.getEvent());
   }
 }
