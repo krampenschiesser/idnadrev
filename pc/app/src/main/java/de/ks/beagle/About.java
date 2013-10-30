80ec26519b08754b7406232c3abf79ff536049a3
scar <krampenschiesser@freenet.de>
2013-10-30 13:03:50 +0100

    use PopupSink in beagle application

--- pc/app/src/main/java/de/ks/beagle/About.java
+++ pc/app/src/main/java/de/ks/beagle/About.java
@@ -5,11 +5,39 @@ package de.ks.beagle;
  * All rights reserved by now, license may come later.
  */
 
+import com.google.common.base.Charsets;
+import com.google.common.io.Files;
 import de.ks.menu.MenuItem;
+import javafx.scene.layout.StackPane;
+import javafx.scene.web.WebView;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
+import java.util.List;
 
 /**
  *
  */
 @MenuItem("/main/help")
-public class About {
+public class About extends StackPane {
+  private static final Logger log = LogManager.getLogger(About.class);
+
+  public About() {
+    WebView webView = new WebView();
+    URL resource = getClass().getResource("about.html");
+    try {
+      StringBuilder builder = new StringBuilder();
+      List<String> lines = Files.readLines(new File(resource.getFile()), Charsets.UTF_8);
+      for (String line : lines) {
+        builder.append(line);
+      }
+      webView.getEngine().loadContent(builder.toString());
+    } catch (IOException e) {
+      log.error("Could not load about.html", e);
+    }
+    getChildren().add(webView);
+  }
 }
--- pc/app/src/main/java/de/ks/beagle/BeagleWindow.java
+++ pc/app/src/main/java/de/ks/beagle/BeagleWindow.java
@@ -7,21 +7,36 @@ package de.ks.beagle;
 
 import de.ks.application.MainWindow;
 import de.ks.menu.presenter.MenuBarPresenter;
+import de.ks.menu.sink.PopupSink;
 import javafx.scene.Parent;
 import javafx.scene.layout.BorderPane;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
+import javax.annotation.PostConstruct;
 import javax.inject.Inject;
 
 /**
  *
  */
 public class BeagleWindow extends MainWindow {
+  private static final Logger log = LogManager.getLogger(BeagleWindow.class);
+
   @Inject
   MenuBarPresenter menuBarPresenter;
+  @Inject
+  PopupSink sink;
+
+  private BorderPane borderPane;
+
+  @PostConstruct
+  public void initialize() {
+    sink.setMenuPath("/main");
+  }
 
   @Override
   public Parent getRoot() {
-    BorderPane borderPane = new BorderPane();
+    borderPane = new BorderPane();
     borderPane.setPrefSize(640, 480);
     borderPane.setTop(menuBarPresenter.getMenu("/main"));
     return borderPane;
@@ -31,4 +46,21 @@ public class BeagleWindow extends MainWindow {
   public String getApplicationTitle() {
     return "Beagle Version 0.3";
   }
+
+
+//  @Subscribe
+//  public void onMenuItemClicked(MenuItemClickedEvent event) {
+//    log.debug("Received event {}",event);
+//    Instance<?> select = CDI.current().select(event.getTarget(),new MenuItemLiteral());
+//    if (select.isUnsatisfied()) {
+//      log.error("No bean satisfying {}", event.getTarget().getName());
+//    } else if (select.isAmbiguous()) {
+//      log.error("Ambigous bean {}", event.getTarget().getName());
+//    } else {
+//      Object object = select.get();
+//      if (object instanceof Node) {
+//        borderPane.setCenter((Node) object);
+//      }
+//    }
+//  }
 }
