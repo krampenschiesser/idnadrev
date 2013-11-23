06b575df9e841cd6064003855f3f63a59b55994c
scar <krampenschiesser@freenet.de>
2013-11-23 10:15:47 +0100

    use invokeInJFXThread

--- pc/libs/application/src/main/java/de/ks/editor/AbstractEditor.java
+++ pc/libs/application/src/main/java/de/ks/editor/AbstractEditor.java
@@ -13,31 +13,16 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 import java.lang.reflect.Field;
-import java.util.concurrent.BrokenBarrierException;
-import java.util.concurrent.CyclicBarrier;
 
 public abstract class AbstractEditor implements Grid2DEditorProvider<Label, Node> {
   private static final Logger log = LogManager.getLogger(AbstractEditor.class);
   protected Label descriptor = new Label();
   protected Field field;
-  private CyclicBarrier barrier = new CyclicBarrier(2);
 
   public AbstractEditor() {
-    ExecutorService.instance.executeInJavaFXThread(() -> {
+    ExecutorService.instance.invokeInJavaFXThread(() -> {
       initializeInJFXThread();
-      try {
-        barrier.await();
-      } catch (InterruptedException | BrokenBarrierException e) {
-        log.error("Failed to wait for barrier.", e);
-        throw new RuntimeException(e);
-      }
     });
-    try {
-      barrier.await();
-    } catch (InterruptedException | BrokenBarrierException e) {
-      log.error("Failed to wait for barrier.", e);
-      throw new RuntimeException(e);
-    }
   }
 
   protected void initializeInJFXThread() {
