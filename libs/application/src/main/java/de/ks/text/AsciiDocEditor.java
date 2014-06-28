fae1cbf80f118383d9b539571116768bea872667
krampenschiesser <krampenschiesser@gmail.com>
2014-06-28 21:31:22 +0200

    more work on asciidoc editor

--- libs/application/src/main/java/de/ks/text/AsciiDocEditor.java
+++ libs/application/src/main/java/de/ks/text/AsciiDocEditor.java
@@ -14,33 +14,75 @@
  */
 package de.ks.text;
 
+import de.ks.activity.ActivityController;
+import de.ks.activity.initialization.ActivityInitialization;
 import de.ks.activity.initialization.LoadInFXThread;
+import de.ks.application.fxml.DefaultLoader;
+import de.ks.i18n.Localized;
+import de.ks.javafx.FxCss;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.value.ObservableValue;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
 import javafx.scene.control.Button;
-import javafx.scene.control.TextArea;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.RowConstraints;
+import javafx.scene.layout.StackPane;
 import javafx.scene.web.WebView;
 import javafx.stage.FileChooser;
+import org.controlsfx.dialog.Dialog;
+import org.fxmisc.richtext.StyleClassedTextArea;
 
+import javax.enterprise.inject.Instance;
+import javax.enterprise.inject.spi.CDI;
+import javax.inject.Inject;
 import java.io.File;
 import java.net.URL;
 import java.util.ResourceBundle;
+import java.util.concurrent.CompletableFuture;
+import java.util.function.Consumer;
 
 @LoadInFXThread
 public class AsciiDocEditor implements Initializable {
+  public static CompletableFuture<DefaultLoader<Node, AsciiDocEditor>> load(Consumer<StackPane> viewConsumer, Consumer<AsciiDocEditor> controllerConsumer) {
+    ActivityInitialization initialization = CDI.current().select(ActivityInitialization.class).get();
+    return initialization.loadAdditionalController(AsciiDocEditor.class)//
+            .thenApply(loader -> {
+              viewConsumer.accept((StackPane) loader.getView());
+              controllerConsumer.accept(loader.getController());
+              return loader;
+            });
+  }
 
+  @Inject
+  @FxCss
+  Instance<String> stylesheets;
+  @Inject
+  AsciiDocParser parser;
+  @Inject
+  ActivityController controller;
   @FXML
-  protected TextArea editor;
+  private StyleClassedTextArea editor;
   @FXML
   protected WebView preview;
   @FXML
   protected Button help;
   @FXML
-  protected Button save;
+  protected Button saveToFileButton;
+  @FXML
+  protected GridPane mainPane;
+
+  protected Dialog helpDialog;
+  protected WebView helpView;
+  protected final SimpleStringProperty text = new SimpleStringProperty();
 
   @Override
   public void initialize(URL location, ResourceBundle resources) {
+    helpView = new WebView();
+    helpView.getEngine().load("http://powerman.name/doc/asciidoc");
 
+    text.bind(editor.textProperty());
   }
 
   @FXML
@@ -56,13 +98,38 @@ public class AsciiDocEditor implements Initializable {
   @FXML
   void saveToFile() {
     FileChooser fileChooser = new FileChooser();
-    File file = fileChooser.showOpenDialog(save.getScene().getWindow());
+    File file = fileChooser.showOpenDialog(saveToFileButton.getScene().getWindow());
 
   }
 
   @FXML
   void showHelp() {
+    helpDialog = new Dialog(this.help, Localized.get("help"));
+    helpDialog.setContent(helpView);
+    helpDialog.show();
+    Instance<String> styleSheets = CDI.current().select(String.class, FxCss.LITERAL);
+    styleSheets.forEach((sheet) -> {
+      helpDialog.getStylesheets().add(sheet);
+    });
 
   }
 
+  public ObservableValue<String> textProperty() {
+    return text;
+  }
+
+  public String getText() {
+    return text.get();
+  }
+
+  public void setText(String text) {
+    this.editor.replaceText(text);
+  }
+
+  public void hideActionBar() {
+    RowConstraints rowConstraints = mainPane.getRowConstraints().get(1);
+    rowConstraints.setMinHeight(0.0);
+    rowConstraints.setPrefHeight(0.0);
+    rowConstraints.setMaxHeight(0.0);
+  }
 }
