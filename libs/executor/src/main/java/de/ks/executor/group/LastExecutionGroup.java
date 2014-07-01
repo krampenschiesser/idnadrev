1c35450a50f23ede8e4d6836af34e4ed2e4c6757
krampenschiesser <krampenschiesser@gmail.com>
2014-07-01 19:49:29 +0200

    added last execution group

---
+++ libs/executor/src/main/java/de/ks/executor/group/LastExecutionGroup.java
@@ -0,0 +1,113 @@
+/*
+ * Copyright [2014] [Christian Loehnert, krampenschiesser@gmail.com]
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package de.ks.executor.group;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.LinkedBlockingDeque;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.function.Supplier;
+
+public class LastExecutionGroup<T> implements Runnable {
+  static class LastExecutionEvent<T> {
+    protected final long scheduleTime;
+    private final Supplier<T> delegate;
+
+    public LastExecutionEvent(Supplier<T> delegate) {
+      this.delegate = delegate;
+      this.scheduleTime = System.currentTimeMillis();
+    }
+
+    public long getScheduleTime() {
+      return scheduleTime;
+    }
+
+    public Supplier<T> getDelegate() {
+      return delegate;
+    }
+  }
+
+  private static final Logger log = LoggerFactory.getLogger(LastExecutionGroup.class);
+
+  protected final LinkedBlockingDeque<LastExecutionEvent<T>> queue = new LinkedBlockingDeque<>();
+  protected final long waitTime;
+  protected ExecutorService executor;
+  protected final AtomicBoolean running = new AtomicBoolean(false);
+
+  protected volatile CompletableFuture<T> result;
+
+  public LastExecutionGroup(long waitTime, ExecutorService executor) {
+    this.waitTime = waitTime;
+    this.executor = executor;
+  }
+
+  public CompletableFuture<T> schedule(Supplier<T> supplier) {
+    queue.add(new LastExecutionEvent<T>(supplier));
+    if (running.compareAndSet(false, true)) {
+      result = new CompletableFuture<>();
+      executor.submit(this);
+    }
+    return result;
+  }
+
+  public void setExecutor(ExecutorService executor) {
+    this.executor = executor;
+  }
+
+  public void run() {
+    try {
+      T value = null;
+      while (!queue.isEmpty()) {
+        T nextValue = getValue();
+        if (nextValue != null) {
+          value = nextValue;
+        }
+      }
+      result.complete(value);
+    } finally {
+      running.set(false);
+    }
+  }
+
+  protected T getValue() {
+    LastExecutionEvent<T> lastEvent = null;
+    while (true) {
+      try {
+        LastExecutionEvent<T> poll = queue.poll(waitTime, TimeUnit.MILLISECONDS);
+        if (poll == null) {
+          if (lastEvent != null) {
+            T value = lastEvent.getDelegate().get();
+            return value;
+          } else {
+            return null;
+          }
+        } else {
+          lastEvent = poll;
+        }
+      } catch (InterruptedException e) {
+        log.error("Got interrupted while polling on queue", e);
+        if (lastEvent != null) {
+          return lastEvent.getDelegate().get();
+        } else {
+          return null;
+        }
+      }
+    }
+  }
+}
---
+++ libs/executor/src/test/java/de/ks/executor/group/LastExecutionGroupTest.java
@@ -0,0 +1,119 @@
+/*
+ * Copyright [2014] [Christian Loehnert, krampenschiesser@gmail.com]
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package de.ks.executor.group;
+
+import org.hamcrest.Matchers;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.concurrent.Callable;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.atomic.LongAdder;
+
+import static org.junit.Assert.*;
+
+public class LastExecutionGroupTest {
+
+  private ExecutorService executorService;
+  private LastExecutionGroup<Integer> lastExecutionGroup;
+
+  @Before
+  public void setUp() throws Exception {
+    executorService = Executors.newFixedThreadPool(2);
+    lastExecutionGroup = new LastExecutionGroup<Integer>(50, executorService);
+    warmUpPool();
+  }
+
+  @After
+  public void tearDown() throws Exception {
+    executorService.shutdownNow();
+  }
+
+  @Test
+  public void testMultipleEvents() throws Exception {
+    int total = 30;
+
+    LongAdder adder = new LongAdder();
+    CompletableFuture<Integer> future = null;
+
+    long startTime = System.currentTimeMillis();
+    for (int i = 0; i < total; i++) {
+      final int number = i;
+      future = lastExecutionGroup.schedule(() -> this.sleep(number + 1, 100, adder));
+    }
+    assertNotNull(future);
+    Integer result = future.get();
+    long stopTime = System.currentTimeMillis();
+    assertNotNull(result);
+
+
+    assertEquals(total, result.intValue());
+
+    assertEquals(1, adder.sum());
+    assertThat(stopTime - startTime, Matchers.lessThan(200L));
+  }
+
+  @Test
+  public void testEventLater() throws Exception {
+    LongAdder adder = new LongAdder();
+
+    lastExecutionGroup.schedule(() -> this.sleep(1, 100, adder));
+    Thread.sleep(110);
+    CompletableFuture<Integer> future = lastExecutionGroup.schedule(() -> this.sleep(2, 100, adder));
+
+    assertNotNull(future);
+    Integer result = future.get();
+    assertNotNull(result);
+
+    assertEquals(2, adder.sum());
+  }
+
+  @Test
+  public void testWaitTime() throws Exception {
+    long startTime = System.currentTimeMillis();
+    CompletableFuture<Integer> future = lastExecutionGroup.schedule(() -> this.sleep(0, 0, null));
+    long stopTime = System.currentTimeMillis();
+    assertThat(stopTime - startTime, Matchers.lessThan(55L));
+  }
+
+  protected void warmUpPool() throws InterruptedException {
+    List<Callable<Integer>> tasks = Arrays.asList(createSleepingCallable(10), createSleepingCallable(10), createSleepingCallable(10), createSleepingCallable(10));
+    executorService.invokeAll(tasks);
+  }
+
+  public Callable<Integer> createSleepingCallable(int timeout) {
+    return () -> sleep(0, 10, null);
+  }
+
+  public int sleep(int number, int timeout, LongAdder adder) {
+    if (timeout > 0) {
+      try {
+        Thread.sleep(timeout);
+      } catch (InterruptedException e) {
+        //
+      }
+    }
+    if (adder != null) {
+      adder.increment();
+    }
+    return number;
+  }
+}
