89985a18ba0a784a7a8e5e492a125c0b10030e18
krampenschiesser <krampenschiesser@gmail.com>
2014-07-17 19:27:41 +0200

    added first testfx test

--- app/src/test/java/de/ks/integration/SimpleWorkflowTest.java
+++ app/src/test/java/de/ks/integration/SimpleWorkflowTest.java
@@ -15,54 +15,72 @@
 package de.ks.integration;
 
 import de.ks.LauncherRunner;
+import de.ks.activity.ActivityController;
+import de.ks.application.Navigator;
+import de.ks.idnadrev.IdnadrevWindow;
 import de.ks.idnadrev.entity.Thought;
+import de.ks.idnadrev.thought.collect.AddThought;
 import de.ks.idnadrev.thought.collect.ThoughtActivity;
 import de.ks.launch.JavaFXService;
 import de.ks.launch.Launcher;
-import de.ks.menu.MenuItem;
 import de.ks.reflection.PropertyPath;
+import de.ks.util.FXPlatform;
 import javafx.scene.Parent;
 import javafx.scene.control.Button;
-import org.junit.Ignore;
+import javafx.scene.layout.BorderPane;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.loadui.testfx.GuiTest;
-import org.loadui.testfx.utils.FXTestUtils;
-import org.loadui.testfx.utils.UserInputDetector;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.enterprise.inject.spi.CDI;
 
 import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertEquals;
 import static org.loadui.testfx.Assertions.verifyThat;
 import static org.loadui.testfx.controls.impl.VisibleNodesMatcher.visible;
 
 @RunWith(LauncherRunner.class)
 public class SimpleWorkflowTest extends GuiTest {
+  private static final Logger log = LoggerFactory.getLogger(SimpleWorkflowTest.class);
   private static final String name = PropertyPath.property(Thought.class, t -> t.getName());
+  private Parent rootNode;
+  private IdnadrevWindow mainWindow;
 
-  @Override
-  public void setupStage() throws Throwable {
-    stage = Launcher.instance.getService(JavaFXService.class).getStage();
-    FXTestUtils.bringToFront(stage);
-    UserInputDetector.instance.reset();
+  @Before
+  public void setupGuiTest() throws Exception {
+    primaryStage = Launcher.instance.getService(JavaFXService.class).getStage();
+    FXPlatform.invokeLater(() -> primaryStage.setScene(null));
+    setupStages(this);
   }
 
   @Override
   protected Parent getRootNode() {
-    return stage.getScene().getRoot();
+    if (rootNode == null) {
+      mainWindow = CDI.current().select(IdnadrevWindow.class).get();
+      BorderPane node = (BorderPane) mainWindow.getNode();
+      Navigator.registerWithExistingPane(primaryStage, node);
+      rootNode = node;
+    }
+    return rootNode;
   }
 
   @Test
-  @Ignore
   public void testCompleteSimpleWorkflow() throws Exception {
-    waitUntil("#" + ThoughtActivity.class.getAnnotation(MenuItem.class).value(), is(visible()));
-    click("#" + ThoughtActivity.class.getAnnotation(MenuItem.class).value());
-    click("#" + ThoughtActivity.class.getAnnotation(MenuItem.class).value() + "/thoughtactivity");
-    waitUntil("#" + name, is(visible()));
-    click("#" + name);
+    ActivityController activityController = CDI.current().select(ActivityController.class).get();
+    assertEquals(ThoughtActivity.class, activityController.getCurrentActivity().getClass());
+
+    AddThought addThought = activityController.getCurrentController();
+
+
+    waitUntil("#name", is(visible()));
+    clickOn("#name");
     verifyThat("#save", (Button s) -> s.isDisabled());
 
     type("Hello Sauerland");
     verifyThat("#save", (Button s) -> !s.isDisabled());
-  }
 
-//  protected void clickMenuItem()
+  }
 }
