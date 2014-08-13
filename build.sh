1661a8b77cb4bb2c0ceb05e8411bd90c416bb2bf
krampenschiesser <krampenschiesser@gmail.com>
2014-08-13 20:04:07 +0200

    don't kill xvfb, since jenkins is running

--- build.sh
+++ build.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
-killall Xvfb
-rm -f /tmp/.X99-lock
-mkdir -p /tmp/xvfb_test
-Xvfb :99 -fbdir /tmp/xvfb_test -ac -extension GLX -extension "Genic Events" &
+if [ ! -f /tmp/.X99-lock ]
+then
+    mkdir -p /tmp/xvfb_test
+    Xvfb :99 -fbdir /tmp/xvfb_test -ac -extension GLX -extension "Genic Events" &
+fi
 export DISPLAY=:99.0
 gradle clean build
