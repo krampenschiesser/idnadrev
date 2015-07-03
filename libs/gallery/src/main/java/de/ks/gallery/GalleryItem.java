4fdc555592a43f85e429d3505e8cfe4994919ae0
krampenschiesser <krampenschiesser@gmail.com>
2015-07-03 18:24:04 +0200

    worked on gallery stuff

---
+++ libs/gallery/src/main/java/de/ks/gallery/GalleryItem.java
@@ -0,0 +1,66 @@
+/**
+ * Copyright [2015] [Christian Loehnert]
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package de.ks.gallery;
+
+import javafx.embed.swing.SwingFXUtils;
+import javafx.scene.image.Image;
+import javafx.scene.image.WritableImage;
+import org.apache.sanselan.ImageReadException;
+
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+public class GalleryItem {
+  protected final File file;
+  protected final String name;
+  protected final Image image;
+  protected final Image thumbNail;
+
+  public GalleryItem(File file, int thumbnailSize) throws IOException, ImageReadException {
+    if (file == null) {
+      throw new IllegalArgumentException("Cannot create from null file ");
+    }
+    if (!file.exists()) {
+      throw new IllegalArgumentException("Cannot create from non-existing file " + file);
+    }
+    this.file = file;
+
+    ImageScaler scaler = new ImageScaler();
+    BufferedImage originalRotated = scaler.rotate(file);
+    BufferedImage thumbNailImage = scaler.rotateAndScale(file, thumbnailSize);
+
+    this.image = SwingFXUtils.toFXImage(originalRotated, new WritableImage(originalRotated.getWidth(), originalRotated.getHeight()));
+    this.thumbNail = SwingFXUtils.toFXImage(thumbNailImage, new WritableImage(thumbNailImage.getWidth(), thumbNailImage.getHeight()));
+    name = file.getName();
+  }
+
+  public File getFile() {
+    return file;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public Image getImage() {
+    return image;
+  }
+
+  public Image getThumbNail() {
+    return thumbNail;
+  }
+}
--- libs/gallery/src/main/java/de/ks/gallery/GalleryResource.java
+++ libs/gallery/src/main/java/de/ks/gallery/GalleryResource.java
@@ -15,63 +15,148 @@
  */
 package de.ks.gallery;
 
-import javafx.collections.ObservableList;
-import javafx.scene.image.Image;
-import org.apache.sanselan.formats.gif.ImageDescriptor;
+import de.ks.activity.executor.ActivityExecutor;
+import de.ks.executor.JavaFXExecutorService;
+import de.ks.option.Options;
+import javafx.collections.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import javax.inject.Inject;
 import java.io.File;
 import java.io.IOException;
-import java.nio.file.FileSystems;
-import java.nio.file.WatchService;
-import java.util.Collection;
+import java.nio.file.*;
+import java.util.*;
+import java.util.concurrent.CompletableFuture;
+import java.util.function.Supplier;
 
 public class GalleryResource {
   private static final Logger log = LoggerFactory.getLogger(GalleryResource.class);
 
-  private final WatchService watchService;
+  protected final ObservableSet<File> files = FXCollections.observableSet(new LinkedHashSet<File>());
+  protected final ObservableMap<File, GalleryItem> file2item = FXCollections.observableMap(new LinkedHashMap<File, GalleryItem>());
+  protected final ObservableList<GalleryItem> items = FXCollections.observableArrayList();
 
-  public static GalleryResource fromFiles(Collection<File> files) {
-    return new GalleryResource();
+  protected Supplier<GallerySettings> settingsSupplier = () -> Options.get(GallerySettings.class);
+  protected final Set<File> parents = new HashSet<>();
+
+  @Inject
+  ActivityExecutor executor;
+  @Inject
+  JavaFXExecutorService javaFXExecutorService;
+
+  private WatchService watchService;
+  private Thread watchThread;
+
+  protected GalleryResource() {
+    clear();
+    files.addListener((SetChangeListener<File>) c -> {
+      int thumbNailSize = settingsSupplier.get().getThumbNailSize();
+
+      HashSet<File> oldParents = new HashSet<File>(parents);
+      HashSet<File> newParents = new HashSet<File>();
+
+      for (File file : this.files) {
+        newParents.add(file.getParentFile());
+
+        if (!file2item.containsKey(file)) {
+          createFile(file, thumbNailSize);
+        }
+      }
+      if (watchService != null) {
+        newParents.removeAll(oldParents);
+        for (File file : newParents) {
+          try {
+            file.toPath().register(watchService, StandardWatchEventKinds.ENTRY_MODIFY, StandardWatchEventKinds.ENTRY_DELETE);
+          } catch (IOException e) {
+            log.error("Could not register {} at watchService.", file, e);
+          }
+        }
+      }
+    });
   }
 
-  public static GalleryResource fromFolder(File folder, boolean recurse) {
-    return new GalleryResource();
+  protected void createFile(File file, int thumbNailSize) {
+    CompletableFuture<GalleryItem> future = CompletableFuture.supplyAsync(() -> {
+      try {
+        GalleryItem descriptor = new GalleryItem(file, thumbNailSize);
+        return descriptor;
+      } catch (Exception e) {
+        log.info("Could not get image descriptor for {}", file, e);
+        throw new RuntimeException(e);
+      }
+    }, executor);
+
+    future.thenAcceptAsync(desc -> {
+      File rf = desc.getFile();
+      if (!file2item.containsKey(file)) {
+        file2item.put(rf, desc);
+        items.add(desc);
+      }
+    }, javaFXExecutorService);
   }
 
-  public static GalleryResource fromImages(Collection<Image> images) {
-    return new GalleryResource();
+  public void setFiles(Collection<File> files) {
+    javaFXExecutorService.submit(() -> {
+      clear();
+      this.files.addAll(files);
+    });
   }
 
-  private GalleryResource() {
-    WatchService service;
+  public void clear() {
+    this.files.clear();
+    file2item.clear();
+    items.clear();
     try {
-      service = FileSystems.getDefault().newWatchService();
+      watchService.close();
     } catch (IOException e) {
-      service = null;
+      log.error("Could not close watchService", e);
     }
-    watchService = service;
+    watchService = null;
+    try {
+      watchService = FileSystems.getDefault().newWatchService();
+      watchThread = new Thread(this::pollService);
+      watchThread.setDaemon(true);
+      watchThread.start();
+    } catch (IOException e) {
+      log.error("Could not open watchService", e);
+    }
   }
 
-  public boolean isVirtualImageNames() {
-    return false;//if we only use javafx images
+  protected void pollService() {
+    WatchKey key = watchService.poll();
+    if (key.isValid()) {
+      List<WatchEvent<?>> watchEvents = key.pollEvents();
+      for (WatchEvent<?> watchEvent : watchEvents) {
+        final WatchEvent<Path> wePath = (WatchEvent<Path>) watchEvent;
+        Path path = wePath.context();
+        File file = path.toFile();
+
+        if (watchEvent.kind() == StandardWatchEventKinds.ENTRY_DELETE) {
+          handleItemDeleted(file);
+        } else {
+          handleItemModified(file);
+        }
+      }
+    }
   }
 
-  public ObservableList<Image> getImages() {
-    return null;
+  private void handleItemModified(File file) {
+    int thumbNailSize = settingsSupplier.get().getThumbNailSize();
+    handleItemDeleted(file);
+    createFile(file, thumbNailSize);
   }
 
-  public ObservableList<String> getImageNames() {
-    return null;
+  private void handleItemDeleted(File file) {
+    if (file2item.containsKey(file)) {
+      javaFXExecutorService.submit(() -> {
+        GalleryItem descriptor = file2item.remove(file);
+        items.remove(descriptor);
+      });
+    }
   }
 
-  public ObservableList<Image> getThumbnails() {
-    return null;
+  public ObservableList<GalleryItem> getItems() {
+    return FXCollections.unmodifiableObservableList(items);
   }
-
-  public ObservableList<ImageDescriptor> getImageDescriptors() {
-    return null;
-  }
-
 }
